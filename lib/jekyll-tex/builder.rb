9163843e296b0b2c655a62f6d90058b5f061d64e
Yan <yansongke1@gmail.com>
2019-06-01 18:24:06 -0700

    Update on touched tex

--- lib/jekyll-tex/builder.rb
+++ lib/jekyll-tex/builder.rb
@@ -6,7 +6,6 @@ module Jekyll
       include Errors
 
       safe true
-      priority :lowest
 
       CONFIG_KEY = "tex".freeze
   
@@ -14,7 +13,7 @@ module Jekyll
         @site = site
 
         site_config = @site.config[CONFIG_KEY] || {}
-        @config = default_config.merge!(site_config.transform_keys(&:to_sym))
+        @config = DEFAULT_CONFIG.merge(site_config.transform_keys(&:to_sym))
 
         Dir.glob(File.join(@site.source, @config[:source]) + '/*.tex') do |path|
           tex = File.basename(path)
@@ -28,26 +27,35 @@ module Jekyll
 
       private
 
-      def update_pdf?(tex)
-        # TODO: Check if the PDF already exists and if it is up-to-date based on
-        # modified timestamp.
-        true
+      def source_path(tex)
+        File.join(@site.source, @config[:source], tex)
       end
 
-      def default_config
-        {
-          builder: 'pdflatex',
-          options: ['--interaction=batchmode'],
-          source:  'assets/tex',
-          output:  'assets',
-        }
+      def target_path(tex)
+        pdf_file = File.basename(tex, '.tex') + '.pdf'
+        File.join(@site.source, @config[:output], pdf_file)
       end
+
+      def update_pdf?(tex)
+        return true unless File.exists?(target_path(tex))
+
+        # Check if the file is up-to-date compared to the tex file. We
+        # want to update if the PDF file is not as recently modified.
+        File.mtime(target_path(tex)) < File.mtime(source_path(tex))
+      end
+
+      # TODO: Make the constants class level.
+      DEFAULT_CONFIG = {
+        builder: 'pdflatex',
+        options: ['--interaction=batchmode'],
+        source:  'assets/tex',
+        output:  'assets',
+      }.freeze
 
       def build(tex)
-        system @config[:builder], *@config[:options], File.join(@site.source, @config[:source], tex)
+        system @config[:builder], *@config[:options], source_path(tex)
 
-        pdf_file    = File.basename(tex, '.tex') + '.pdf'
-        target_path = File.join(@site.source, @config[:output], pdf_file)
+        pdf_file = File.basename(tex, '.tex') + '.pdf'
 
         # The builder outputs the PDF file in the top level directory of the site. We need to
         # move it to the source directory, and add it to the site's inventory.
@@ -55,7 +63,7 @@ module Jekyll
           raise BuildError
         end
 
-        FileUtils.move pdf_file, target_path
+        FileUtils.move pdf_file, target_path(tex)
         @site.static_files << StaticFile.new(@site, @site.source, @config[:output], pdf_file)
       end
 
